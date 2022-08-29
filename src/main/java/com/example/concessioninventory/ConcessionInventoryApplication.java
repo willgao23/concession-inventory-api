66f720c5b9dd1d485330d92f65fba669405419ca
wgao23 <wgao23@students.cs.ubc.ca>
2022-08-29 11:29:44 -0700

    Create project starter with Spring Initializr

---
+++ src/main/java/com/example/concessioninventory/ConcessionInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.concessioninventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concessioninventory/ConcessionInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concessioninventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
