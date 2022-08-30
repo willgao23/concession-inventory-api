b30be538729af48507cf951e6b54f0d2ff122d80
wgao23 <wgao23@students.cs.ubc.ca>
2022-08-30 01:07:25 -0700

    Add class level comments to snack category and repository

--- src/main/java/com/example/concessioninventory/snack/SnackCategory.java
+++ src/main/java/com/example/concessioninventory/snack/SnackCategory.java
@@ -1,5 +1,6 @@
 package com.example.concessioninventory.snack;
 
+// Represents the different categories a snack can belong to
 public enum SnackCategory {
     Salty, Sweet, Drink
 }
--- src/main/java/com/example/concessioninventory/snack/SnackRepository.java
+++ src/main/java/com/example/concessioninventory/snack/SnackRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// Represents the database layer where snacks are stored
 public interface SnackRepository extends JpaRepository<Snack, Long> {
     Optional<Snack> findSnackByName(String name);
 }
