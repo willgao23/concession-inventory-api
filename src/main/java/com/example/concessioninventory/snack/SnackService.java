c22eb7ee3e47ca4bc1047b9cec8942a7a94688a6
wgao23 <wgao23@students.cs.ubc.ca>
2022-08-29 17:34:58 -0700

    Implement get functionality

--- src/main/java/com/example/concessioninventory/snack/SnackService.java
+++ src/main/java/com/example/concessioninventory/snack/SnackService.java
@@ -1,14 +1,24 @@
 package com.example.concessioninventory.snack;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// Represents the business logic layer of the concession stand API
 @Service
 public class SnackService {
 
+    private SnackRepository snackRepository;
+
+    //EFFECTS: constructs a snack service with the given snack repository
+    @Autowired
+    public void SnackService(SnackRepository snackRepository) {
+        this.snackRepository = snackRepository;
+    }
+
+    //EFFECTS: returns a list of all snacks inside the repository
     public List<Snack> getSnacks() {
-        return List.of(new Snack("Peanut M&Ms", 1.99, SnackCategory.Sweet, true,
-                false, false, 15));
+        return snackRepository.findAll();
     }
 }
