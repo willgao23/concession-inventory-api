46578f1db4b032443f976956670f6d6d022e4c22
wgao23 <wgao23@students.cs.ubc.ca>
2022-08-29 17:35:38 -0700

    Add comments to snack controller

--- src/main/java/com/example/concessioninventory/snack/SnackController.java
+++ src/main/java/com/example/concessioninventory/snack/SnackController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// Represents the API layer of the concession stand API
 @RestController
 @RequestMapping (path = "concession-inventory-api/v1")
 public class SnackController {
 
     private SnackService snackService;
 
+    //EFFECTS: constructs a snack controller with the given snack service
     @Autowired
     public void SnackController(SnackService snackService) {
         this.snackService = snackService;
     }
 
+    //EFFECTS: returns a list of snacks from the database
     @GetMapping
     public List<Snack> getSnacks() {
         return snackService.getSnacks();
